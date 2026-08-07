0a8917caee8ee2d6163b1269c555a5b5ab5b3a72
Siddharth Gajbhare <160532097+siddharthgajbhare@users.noreply.github.com>
2026-08-07 11:06:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//comit
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('contactForm');
     const successMessage = document.getElementById('successMessage');
